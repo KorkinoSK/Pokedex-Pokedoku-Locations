a56c1fdf6b3ed4097a157caced04d0d3c6ad1e3c
KorkinoSK <unknownkorkino@gmail.com>
2026-04-11 20:34:51 +0200

    Add Pokémon lookup and input placeholder handling
    Introduce pokemonUrl and pokemon refs and wire up a basic fetch in checkTheAnswer to query the Pokémon API. Bind the placeholder ref to all search input fields so contextual messages can be shown, clear the pokemon input when starting a search, and set a user-facing placeholder message when a Pokémon is not found. Includes a try/catch around the fetch and a console.log placeholder for WIP response handling.

--- pokedex/src/pokedoku.vue
+++ pokedex/src/pokedoku.vue
@@ -2,6 +2,7 @@
 import { ref } from "vue";
 const pokedokuIsDone = ref(false)
 const url = ref("https://pokeapi.co/api/v2/type")
+const pokemonUrl = ref("https://pokeapi.co/api/v2/pokemon-form/")
 const types = ref(getTypes())
 const chosenTypes = ref(["","","","","",""])
 const attemptsLeft = ref(0)
@@ -10,6 +11,7 @@ const grid = ref(
     'unknown', 'unknown', 'unknown',
     'unknown', 'unknown', 'unknown']
 )
+
 const pokemon = ref('')
 const placeholder = ref("Skús napr. bulbasaurus.")
 function getTypes() {
@@ -38,10 +40,20 @@ function generateNewPokedoku() {
     }
 }
 function checkTheAnswer() {
-    
+    try {
+    fetch(pokemonUrl.value + pokemon.value.toLowerCase())
+    .then((res)=>(res.json()))
+    .then((json) => {
+        console.log("WIP")
+        }
+    )
+} catch(error) {
+    thisPokemonDoesNotExist()
+}
 }
 function thisPokemonDoesNotExist() {
-    
+    pokemon.value = ''
+    placeholder = 'Tento pokémon neexistuje. Možno si ho zle napísal?'
 }
 function thisPokemonIsNotCorrect() {
     
@@ -65,9 +77,11 @@ function startSearching(index) {
         }
     }
     grid.value[index] = 'searching'
+    pokemon.value = ''
 }
 function stopSearching(index) {
     grid.value[index] = 'unknown'
+    
 }
 
 generateNewPokedoku()
@@ -87,7 +101,7 @@ generateNewPokedoku()
                 <img @click="startSearching(0)" v-if="grid[0] == 'unknown'" 
                 src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpngimg.com%2Fuploads%2Fquestion_mark%2Fquestion_mark_PNG70.png&f=1&nofb=1&ipt=707fd30ce8f04457c290fe6a553bd687998a973f8add407247101e8b3299166e" width="100px" height="100px">
                 <div class="searching" v-else-if="grid[0] == 'searching'">
-                    <input type="text" v-model="pokemon">
+                    <input type="text" v-model="pokemon" :placeholder="placeholder">
                     <button @click="checkTheAnswer(0, 3)">Vyhľadaj</button>
                     <button @click="stopSearching(0)">Odskoč si</button>
                 </div>
@@ -96,7 +110,7 @@ generateNewPokedoku()
                 <img @click="startSearching(3)" v-if="grid[3] == 'unknown'" 
                 src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpngimg.com%2Fuploads%2Fquestion_mark%2Fquestion_mark_PNG70.png&f=1&nofb=1&ipt=707fd30ce8f04457c290fe6a553bd687998a973f8add407247101e8b3299166e" width="100px" height="100px">
                 <div class="searching" v-else-if="grid[3] == 'searching'">
-                    <input type="text" v-model="pokemon">
+                    <input type="text" v-model="pokemon" :placeholder="placeholder">
                     <button @click="checkTheAnswer(1,3)">Vyhľadaj</button>
                     <button @click="stopSearching(3)">Odskoč si</button>
                 </div>
@@ -105,7 +119,7 @@ generateNewPokedoku()
                 <img @click="startSearching(6)" v-if="grid[6] == 'unknown'" 
                 src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpngimg.com%2Fuploads%2Fquestion_mark%2Fquestion_mark_PNG70.png&f=1&nofb=1&ipt=707fd30ce8f04457c290fe6a553bd687998a973f8add407247101e8b3299166e" width="100px" height="100px">
                 <div class="searching" v-else-if="grid[6] == 'searching'">
-                    <input type="text" v-model="pokemon">
+                    <input type="text" v-model="pokemon" :placeholder="placeholder">
                     <button @click="checkTheAnswer(2,3)">Vyhľadaj</button>
                     <button @click="stopSearching(6)">Odskoč si</button>
                 </div>
@@ -117,7 +131,7 @@ generateNewPokedoku()
                 <img @click="startSearching(1)" v-if="grid[1] == 'unknown'" 
                 src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpngimg.com%2Fuploads%2Fquestion_mark%2Fquestion_mark_PNG70.png&f=1&nofb=1&ipt=707fd30ce8f04457c290fe6a553bd687998a973f8add407247101e8b3299166e" width="100px" height="100px">
                 <div class="searching" v-else-if="grid[1] == 'searching'">
-                    <input type="text" v-model="pokemon">
+                    <input type="text" v-model="pokemon" :placeholder="placeholder">
                     <button @click="checkTheAnswer(0,4)">Vyhľadaj</button>
                     <button @click="stopSearching(1)">Odskoč si</button>
                 </div>
@@ -126,7 +140,7 @@ generateNewPokedoku()
                 <img @click="startSearching(4)" v-if="grid[4] == 'unknown'" 
                 src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpngimg.com%2Fuploads%2Fquestion_mark%2Fquestion_mark_PNG70.png&f=1&nofb=1&ipt=707fd30ce8f04457c290fe6a553bd687998a973f8add407247101e8b3299166e" width="100px" height="100px">
                 <div class="searching" v-else-if="grid[4] == 'searching'">
-                    <input type="text" v-model="pokemon">
+                    <input type="text" v-model="pokemon" :placeholder="placeholder">
                     <button @click="checkTheAnswer(1,4)">Vyhľadaj</button>
                     <button @click="stopSearching(4)">Odskoč si</button>
                 </div>
@@ -135,7 +149,7 @@ generateNewPokedoku()
                 <img @click="startSearching(7)" v-if="grid[7] == 'unknown'" 
                 src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpngimg.com%2Fuploads%2Fquestion_mark%2Fquestion_mark_PNG70.png&f=1&nofb=1&ipt=707fd30ce8f04457c290fe6a553bd687998a973f8add407247101e8b3299166e" width="100px" height="100px">
                 <div class="searching" v-else-if="grid[7] == 'searching'">
-                    <input type="text" v-model="pokemon">
+                    <input type="text" v-model="pokemon" :placeholder="placeholder">
                     <button @click="checkTheAnswer(2,4)">Vyhľadaj</button>
                     <button @click="stopSearching(7)">Odskoč si</button>
                 </div>
@@ -147,7 +161,7 @@ generateNewPokedoku()
                 <img @click="startSearching(2)" v-if="grid[2] == 'unknown'" 
                 src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpngimg.com%2Fuploads%2Fquestion_mark%2Fquestion_mark_PNG70.png&f=1&nofb=1&ipt=707fd30ce8f04457c290fe6a553bd687998a973f8add407247101e8b3299166e" width="100px" height="100px">
                 <div class="searching" v-else-if="grid[2] == 'searching'">
-                    <input type="text" v-model="pokemon">
+                    <input type="text" v-model="pokemon" :placeholder="placeholder">
                     <button @click="checkTheAnswer(0,5)">Vyhľadaj</button>
                     <button @click="stopSearching(2)">Odskoč si</button>
                 </div>
@@ -156,7 +170,7 @@ generateNewPokedoku()
                 <img @click="startSearching(5)" v-if="grid[5] == 'unknown'" 
                 src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpngimg.com%2Fuploads%2Fquestion_mark%2Fquestion_mark_PNG70.png&f=1&nofb=1&ipt=707fd30ce8f04457c290fe6a553bd687998a973f8add407247101e8b3299166e" width="100px" height="100px">
                 <div class="searching" v-else-if="grid[5] == 'searching'">
-                    <input type="text" v-model="pokemon">
+                    <input type="text" v-model="pokemon" :placeholder="placeholder">
                     <button @click="checkTheAnswer(1,5)">Vyhľadaj</button>
                     <button @click="stopSearching(5)">Odskoč si</button>
                 </div>
@@ -165,7 +179,7 @@ generateNewPokedoku()
                 <img @click="startSearching(8)" v-if="grid[8] == 'unknown'" 
                 src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpngimg.com%2Fuploads%2Fquestion_mark%2Fquestion_mark_PNG70.png&f=1&nofb=1&ipt=707fd30ce8f04457c290fe6a553bd687998a973f8add407247101e8b3299166e" width="100px" height="100px">
                     <div class="searching" v-else-if="grid[8] == 'searching'">
-                        <input type="text" v-model="pokemon" placeholder= ''>
+                        <input type="text" v-model="pokemon" :placeholder="placeholder">
                         <button @click="checkTheAnswer(2,5)">Vyhľadaj</button>
                         <button @click="stopSearching(8)">Odskoč si</button>
                     </div>
